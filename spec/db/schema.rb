ea6e53905d240fcf0973911c97f5562b8a674fca
Mike Atlas <mikeatlas@gmail.com>
2013-11-04 14:05:03 -0500

    id is created by default

--- spec/db/schema.rb
+++ spec/db/schema.rb
@@ -1,14 +1,12 @@
 ActiveRecord::Schema.define :version => 0 do
 
   create_table :people, :force => true do |t|
-    t.integer :id
     t.string  :name
     t.date    :birthday
     t.float   :height
   end
 
   create_table :stores, :force => true do |t|
-    t.integer :id
     t.string  :name
     t.integer :owner_id
   end
@@ -19,28 +17,24 @@ ActiveRecord::Schema.define :version => 0 do
   end
 
   create_table :apples, :force => true do |t|
-    t.integer :id
     t.string  :name
     t.integer :store_id
     t.integer :person_id
   end
 
   create_table :bananas, :force => true do |t|
-    t.integer :id
     t.string  :name
     t.integer :store_id
     t.integer :person_id
   end
 
   create_table :pears, :force => true do |t|
-    t.integer :id
     t.string  :name
     t.integer :store_id
     t.integer :person_id
   end
 
   create_table :addresses, :force => true do |t|
-    t.integer :id
     t.string  :name
     t.integer :store_id
   end
