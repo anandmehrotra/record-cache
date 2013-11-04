2828747c48bdad430c8a48ca73cf40aa71486f0e
Mike Atlas <mikeatlas@gmail.com>
2013-11-04 12:55:35 -0500

    fix constant re-setting

--- lib/record_cache/datastore/active_record.rb
+++ lib/record_cache/datastore/active_record.rb
@@ -7,14 +7,15 @@ ActiveRecord::Base.send(:include, RecordCache::Base)
 # some internal Active Record methods needs to be aliased.
 # The downside of using internal methods, is that they may change in different releases,
 # hence the following code:
-AR_VERSION = 31
+version = 31
 
 if ActiveRecord::VERSION::MAJOR == 3 && ActiveRecord::VERSION::MINOR == 0
-	AR_VERSION = 30
+	version = 30
 elsif ActiveRecord::VERSION::MAJOR == 3 && ActiveRecord::VERSION::MINOR == 1
-	AR_VERSION = 31	
+	version = 31	
 elsif ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR == 0
-	AR_VERSION = 40
+	version = 40
 end
 
+AR_VERSION = version
 require File.dirname(__FILE__) + "/active_record_#{AR_VERSION}.rb"
